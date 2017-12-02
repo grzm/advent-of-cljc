455fd9b49acb04514967b5d596a1b1084456a289
Michael Glaesemann <grzm@seespotcode.net>
2017-12-02 11:29:55 -0600

    Add tests for Day 1 input data.

--- src/main/com/grzm/advent_of_code/advent_2017/day_01/part_1_test.clj
+++ src/main/com/grzm/advent_of_code/advent_2017/day_01/part_1_test.clj
@@ -1,6 +1,7 @@
 (ns com.grzm.advent-of-code.advent-2017.day-01.part-1-test
   (:require
    [clojure.test :refer [deftest are]]
+   [com.grzm.advent-of-code.advent-2017.day-01.data :as data]
    [com.grzm.advent-of-code.advent-2017.day-01.part-1 :refer [solve]]))
 
 (deftest solve-test
@@ -8,4 +9,5 @@
     3 "1122"
     4 "1111"
     0 "1234"
-    9 "91212129"))
+    9 "91212129"
+    1175  data/input))
--- src/main/com/grzm/advent_of_code/advent_2017/day_01/part_2_test.clj
+++ src/main/com/grzm/advent_of_code/advent_2017/day_01/part_2_test.clj
@@ -1,6 +1,7 @@
 (ns com.grzm.advent-of-code.advent-2017.day-01.part-2-test
   (:require
    [clojure.test :refer [deftest are]]
+   [com.grzm.advent-of-code.advent-2017.day-01.data :as data]
    [com.grzm.advent-of-code.advent-2017.day-01.part-2 :refer [solve]]))
 
 (deftest solve-test
@@ -9,4 +10,5 @@
     0 "1221"
     4 "123425"
     12 "123123"
-    4 "12131415"))
+    4 "12131415"
+    1166 data/input))
